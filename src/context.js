0580b5e949f869e88875c26d8c5f8c2b5a8ca7f0
Vinay <vinay.d.kumar@macys.com>
2020-07-05 12:47:27 +0530

    git todos added

--- src/context.js
+++ src/context.js
@@ -7,9 +7,9 @@ const TodosContext=React.createContext({
         {id:1,text:"Read React", complete:false},
         {id:2,text:"Read React Hooks", complete:false},
         {id:3,text:"Practice Learning", complete:false},
-        {id:4,text:"Do project", complete:false}
-        {id:5,text:"Create Git Repo", complete:false}
-        {id:6,text:"Commit your changes", complete:false}
+        {id:4,text:"Do project", complete:false},
+        {id:5,text:"Create Git Repo", complete:false},
+        {id:6,text:"Commit your changes", complete:false},
         {id:7,text:"Push it to Git Repo", complete:false}
 
     ],
